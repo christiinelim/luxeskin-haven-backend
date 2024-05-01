cc92068e42ee6524abae1fc954d7765433c7577f
christiinelim <christinelim.clp@gmail.com>
2024-05-02 00:45:16 +0800

    added cartout processed route

--- routes/api/cartout.js
+++ routes/api/cartout.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { authenticateWithJWT } = require('../../middlewares');
 const cartServices = require('../../services/cart_service');
 const productServices = require('../../services/product_service');
+const orderServices = require('../../services/order_service');
 const Stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 router.post('/', authenticateWithJWT, async (req, res) => {
@@ -20,7 +21,9 @@ router.post('/', authenticateWithJWT, async (req, res) => {
                     images: [item.product.image],
                     metadata: {
                         product_id: item.product.id,
-                        cart_id: item.id
+                        cart_id: item.id,
+                        quantity: item.quantity,
+                        cost: item.product.cost
                     }
                 }
             }
@@ -45,7 +48,7 @@ router.post('/', authenticateWithJWT, async (req, res) => {
     }
 });
 
-router.post('/webhook', async (req, res) => {
+router.post('/webhooks', async (req, res) => {
     const payload = req.body;
     const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET
 
@@ -65,17 +68,24 @@ router.post('/webhook', async (req, res) => {
                 }
             );
 
+
             const lineItems = await Stripe.checkout.sessions.listLineItems(stripeSession.id, {
                 expand: ['data.price', 'data.price.product']
             });
-            
-            lineItems.data.forEach(async (item) => {
-                // const priceId = item.price.id;
-                // const price = await Stripe.prices.retrieve(priceId);
 
-                // delete from cart
+            let products = [];
+
+            for (const item of lineItems.data) {
                 const productId = item.price.product.id;
                 const product = await Stripe.products.retrieve(productId);
+        
+                const productObject = {
+                    id: parseInt(product.metadata.product_id),
+                    quantity: parseInt(product.metadata.quantity)
+                };
+                products.push(productObject);
+
+                // delete from cart
                 await cartServices.deleteCartItem(product.metadata.cart_id);
 
                 const purchasedQuantity = item.quantity;
@@ -83,12 +93,14 @@ router.post('/webhook', async (req, res) => {
 
                 // update product stock
                 await productServices.updateProductQuantity(purchasedProductId, purchasedQuantity);
-            });
-
+            }
+        
+            // create order
+            await orderServices.createOrder(products, { "user_id": session.metadata.user_id, "total_cost": (session.amount_total) / 100 });
+                    
             res.json({ 'status': 'Success' })
         }
     } catch (error) {
-        console.log(error)
         res.status(500).json({ error })
     }
 })
